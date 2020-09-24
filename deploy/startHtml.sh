29ea47bd1a144e4b61e5e6a205c8d09539acee22
manug <manugarg2@gmail.com>
2020-09-24 14:23:26 +0800

    html-ui start script

--- deploy/startHtml.sh
+++ deploy/startHtml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 ./kubectl -n jenkins apply -f ./deploy/service-html.yaml
-./kubectl -n jenkins apply -f ./deploy/deployment-html.yaml
+cat ./deploy/deployment-html.yaml | sed s/1.0.0/${BUILD_NUMBER}/g | ./kubectl apply -f -
 
 HTML_UI_IP_VALUE=`./kubectl -n jenkins get svc/html-service  --output jsonpath='{.status.loadBalancer.ingress[0].hostname}'`
 echo "html UI started: http://${HTML_UI_IP_VALUE}"
