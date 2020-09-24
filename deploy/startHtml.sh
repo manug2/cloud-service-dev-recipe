2150a6787073d4da3ae77e59368618e5a2a4a21d
manug <manugarg2@gmail.com>
2020-09-24 13:01:30 +0800

    html-ui deployment script

--- deploy/startHtml.sh
+++ deploy/startHtml.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -x
-
-#get external IP of calc service
-CALC_SERVICE_IP_VALUE=`./kubectl -n jenkins get svc/calc-service  --output jsonpath='{.status.loadBalancer.ingress[0].hostname}'`
-echo "calc service extenal IP: '${CALC_SERVICE_IP_VALUE}'"
-
-#deploy ui pods and ui extenal service with knowledge of CALC_SERVICE_IP value
 ./kubectl -n jenkins apply -f ./deploy/service-html.yaml
-cat ./deploy/deployment-html.yaml | sed "s/1.0.0/${BUILD_NUMBER}/g" | sed "s/CALC_SERVICE_IP_VALUE/${CALC_SERVICE_IP_VALUE}/g" >> ./deploy/temp-service-html.yaml
-grep "amazonaws" ./deploy/temp-service-html.yaml
-./kubectl -n jenkins apply -f ./deploy/temp-service-html.yaml
+./kubectl -n jenkins apply -f ./deploy/deployment-html.yaml
 
 HTML_UI_IP_VALUE=`./kubectl -n jenkins get svc/html-service  --output jsonpath='{.status.loadBalancer.ingress[0].hostname}'`
-echo "python UI started: http://${HTML_UI_IP_VALUE}"
+echo "html UI started: http://${HTML_UI_IP_VALUE}"
